1a03915aceb7b97b040a05e68916983194513224
Brian. <97397197+SamQxd@users.noreply.github.com>
2025-06-10 11:21:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ from_currency = 'JPY'
 to_currency = 'EUR'
 converted_amount = c.convert(amount, from_currency, to_currency)
 print(f"{amount} {from_currency} is equal to {converted_amount:.2f} {to_currency}")
+
+#use some library that has the database of all currencies(symbols and abbreviations and names)
+#that make a simple addon fronted code(selected text gets converted)
